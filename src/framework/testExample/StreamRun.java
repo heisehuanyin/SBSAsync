76f1a02633469b905bba934b1669fbde142f0556
W.S <2422523675@qq.com>
2019-10-03 23:15:39 +0800

    test update

--- src/framework/testExample/StreamRun.java
+++ src/framework/testExample/StreamRun.java
@@ -10,17 +10,33 @@ public class StreamRun {
         q.newStream(0, controller -> {
             System.out.println("First");
             controller.checkPass();
+            System.out.println("Fend");
         }).Then(controller -> {
             System.out.println("Second");
             controller.checkPass();
+            System.out.println("Send");
         }).Then(controller -> {
             System.out.println("Third");
             controller.checkPass();
+            System.out.println("Tend");
         }).Then(controller -> {
             System.out.println("4");
             controller.checkPass();
+            System.out.println("4end");
         }).complete();
 
         q.getController(0).run();
+
+        q.newStream(1, controller -> {
+            System.out.println("---1");
+        }).Then(controller -> {
+            System.out.println("----2");
+        }).Then(controller -> {
+            System.out.println("-----4");
+        });
+        q.getController(1).checkPass();
+        q.getController(1).checkPass();
+        q.getController(1).checkPass();
+        q.getController(1).checkPass();
     }
 }
